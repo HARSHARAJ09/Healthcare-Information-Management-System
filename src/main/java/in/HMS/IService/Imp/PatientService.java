26fc3099afaa1fa2f3052d867ac6029ffff166c7
HARSHARAJ09 <harsharajbsy@gmail.com>
2025-12-24 00:36:09 +0530

    updated

--- src/main/java/in/HMS/IService/Imp/PatientService.java
+++ src/main/java/in/HMS/IService/Imp/PatientService.java
@@ -1,7 +1,10 @@
 package in.HMS.IService.Imp;
 
+import org.springframework.stereotype.Service;
+
 import in.HMS.IService.IPatient;
 
+@Service
 public class PatientService implements IPatient{
 
 }
