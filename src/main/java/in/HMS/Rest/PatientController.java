b8482cfe55dd38a1e9c73805cfd3a79f1383ec50
HARSHARAJ09 <harsharajbsy@gmail.com>
2025-12-24 00:21:27 +0530

    updated

---
+++ src/main/java/in/HMS/Rest/PatientController.java
@@ -0,0 +1,10 @@
+package in.HMS.Rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/HMS/patients")
+public class PatientController {
+
+}
