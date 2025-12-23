0bb06f6798aad3947dcf0ad371575a576809fb8d
HARSHARAJ09 <harsharajbsy@gmail.com>
2025-12-23 22:37:58 +0530

    HMS Structure Added

---
+++ src/test/java/in/HMS/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package in.HMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
